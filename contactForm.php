afad012fc484a74de22687e764034c134e31b1eb
Alexander de Orbegoso <95198941+Alexdeor@users.noreply.github.com>
2023-09-14 19:47:34 -0500

    Add files via upload

--- contactForm.php
+++ contactForm.php
@@ -1,6 +1,5 @@
 <?php
 
-
 if(isset($_POST['submit'])){
     $name = $_POST['name'];
     $subject = $_POST['subject'];
@@ -12,6 +11,7 @@ if(isset($_POST['submit'])){
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
+    header("Location: index.html?mailsend");
 
 }
+?>
