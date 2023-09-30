acc801912e467b9d7d21dcd31a8a86ffd0e299d7
Alexander de Orbegoso <95198941+Alexdeor@users.noreply.github.com>
2023-09-29 23:32:11 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -12,51 +12,30 @@ const observer = new IntersectionObserver((entries) =>{
 const hiddenItems = document.querySelectorAll('.hiddenText');
 hiddenItems.forEach((el) => observer.observe(el));
 
-  //Displays the cards content info        
+
 const btn = document.querySelectorAll('.button');
-const cardContent = document.querySelectorAll('.card-content');
-const card = document.querySelectorAll('.card');
-const project = document.querySelector('.project-container');
-const mql = window.matchMedia("(min-width: 1250px))");
 
-//The content will display when the card is targeted
-function moveIn(x){
-  if(x.matches){
-project.addEventListener('mousemove', (e)=>{
-  for (let i = 0; i< card.length; i++){ 
-    if(e.target === card[i] || e.target === cardContent[i]){
-      cardContent[i].style.transform = "translateY(0%)" ;
-      cardContent[i].style.transition = "transform 500ms ease";
-      e.preventDefault();
-    }   
+// Create a MediaQueryList object for the specified media query
+const mediaQuery = window.matchMedia("(min-width: 1200px)");
+
+// Function to be executed when the media query matches
+function handleMediaQuery(event) {
+  if (event.matches) {
+    // Media query condition is met (viewport width is at least 1200px)
+    console.log("Media query Match!");
+    btn.forEach(event =>{
+      event.addEventListener('click', (e) =>{
+        console.log(e)
+      document.location.reload();
+      })});
+      
+  } else {
+    // Media query condition is not met (viewport width is less than 1200px)
+    console.log("Media query does not match!");
+ 
   }
-}) 
-moveOut();
 }
-else{
-  btn.forEach(e =>{e.addEventListener('click', ()=> {window.location.reload();})
-});
-}
-}
-//The content will hide when the mouse is being removed from the target
-function moveOut(){
-  card.forEach(e =>{
-    e.addEventListener('mouseleave', ()=>{
-      cardContent.forEach(e =>{
-        e.style.transform = "translateY(75%)" ;
-        e.style.transition = "transform 500ms ease";
-      })
-    })
-  });
-btn.forEach(e =>{e.addEventListener('click', ()=> {window.location.reload();})
-});
 
-
-
-}
-//media query
-x = window.matchMedia("(min-width: 1250px)")
-moveIn(x) 
-x.addListener(moveIn) 
-
-
+// Initial check of the media query state and attach event listener for changes
+handleMediaQuery(mediaQuery);
+mediaQuery.addListener(handleMediaQuery);
